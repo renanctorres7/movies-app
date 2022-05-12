0d8b1b6af5d58ea627ae898ac8dc13ce675b97e5
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-05-12 08:03:40 -0300

    Create SearchResultsRepositoryMock

--- movies/test/app/features/domain/usecases/search_by_text_test.dart
+++ movies/test/app/features/domain/usecases/search_by_text_test.dart
@@ -1,6 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:movies/app/features/domain/repositories/search_results_repository.dart';
+import 'package:movies/app/features/domain/usecases/search_by_text.dart';
+
+class SearchResultsRepositoryMock extends Mock
+    implements SearchResultsRepository {}
 
 void main() {
-  final repository = SearchResultsRepository();
+  final repository = SearchResultsRepositoryMock();
+  final usecase = SearchByTextImpl();
   test('Should return a list with results', () {});
 }
