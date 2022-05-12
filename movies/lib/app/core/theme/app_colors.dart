97b6b2b067b211b0b9d0eeda74e746493d858dc2
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-05-12 13:35:20 -0300

    Create app colors

---
+++ movies/lib/app/core/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color colorWhite = Color(0xFFFFFFFF);
+  static const Color colorGray01 = Color(0xFF343A40);
+  static const Color colorGray02 = Color(0xFF5E6770);
+  static const Color colorGray03 = Color(0xFF868E96);
+
+  static const Color colorGray08 = Color(0xFFF1F3F5);
+  static const Color colorHighlight = Color(0xFF00384C);
+}
