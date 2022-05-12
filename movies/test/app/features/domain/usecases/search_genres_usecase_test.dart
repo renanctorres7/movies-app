c2c5abf43bb9a5eaa900dd2d294ef0b3469d960b
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-05-12 11:16:04 -0300

    Add test: 'Should return a Server Failure when dont succeed'

--- movies/test/app/features/domain/usecases/search_genres_usecase_test.dart
+++ movies/test/app/features/domain/usecases/search_genres_usecase_test.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:movies/app/features/domain/entities/search_genres.dart';
+import 'package:movies/app/features/domain/errors/errors.dart';
 import 'package:movies/app/features/domain/repositories/search_genres_repository.dart';
 import 'package:movies/app/features/domain/usecases/search_by_text.dart';
 import 'package:movies/app/features/domain/usecases/search_genres_usecase.dart';
@@ -19,5 +20,13 @@ void main() {
     final result = await usecase.call();
 
     expect(result, Right(list));
+  });
+
+  test('Should return a Server Failure when dont succeed', () async {
+    when(() => repository.getGenresList())
+        .thenAnswer((_) async => Left(ServerFailure()));
+    final result = await usecase.call();
+
+    expect(result, Left(ServerFailure()));
   });
 }
