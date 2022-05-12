f029b59724d5abd34deda256e2c8f8dc04a0ae9d
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-05-12 13:23:33 -0300

    Add flutter_screenutil and search page

--- movies/lib/app/app_widget.dart
+++ movies/lib/app/app_widget.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'My Smart App',
-      routeInformationParser: Modular.routeInformationParser,
-      routerDelegate: Modular.routerDelegate,
-    ); //added by extension
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (child) {
+          return MaterialApp.router(
+            title: 'My Smart App',
+            routeInformationParser: Modular.routeInformationParser,
+            routerDelegate: Modular.routerDelegate,
+          );
+        }); //added by extension
   }
 }
