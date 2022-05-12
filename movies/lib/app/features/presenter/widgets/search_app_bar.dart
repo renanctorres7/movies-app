66bcfc9b0bf2af4a78166180a3012fc9a4e20017
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-05-12 14:37:45 -0300

    Initial appbar

--- movies/lib/app/features/presenter/widgets/search_app_bar.dart
+++ movies/lib/app/features/presenter/widgets/search_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movies/app/core/theme/app_colors.dart';
 
 class SearchAppBar extends StatefulWidget {
   const SearchAppBar({Key? key}) : super(key: key);
@@ -13,8 +14,20 @@ class _SearchAppBarState extends State<SearchAppBar> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Container(
+      color: AppColors.colorWhite,
       height: 245.h,
       width: size.width,
+      child: Padding(
+        padding: EdgeInsets.only(top: 48.h),
+        child: Column(
+          children: [
+            Text(
+              'Filmes',
+              style: Theme.of(context).textTheme.headline1,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
