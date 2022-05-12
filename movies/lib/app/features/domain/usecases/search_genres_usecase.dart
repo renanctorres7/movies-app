b42cb368fc6b8ffc36bc1a151085ae4beb06ad83
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-05-12 11:14:47 -0300

    Add test: 'Should receive a Search Genres list'

--- movies/lib/app/features/domain/usecases/search_genres_usecase.dart
+++ movies/lib/app/features/domain/usecases/search_genres_usecase.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:movies/app/features/domain/entities/search_genres.dart';
+import 'package:movies/app/features/domain/repositories/search_genres_repository.dart';
 
 import '../errors/errors.dart';
 
@@ -8,8 +9,11 @@ abstract class SearchGenresUsecase {
 }
 
 class SearchGenresUsecaseImpl implements SearchGenresUsecase {
+  final SearchGenresRepository repository;
+
+  SearchGenresUsecaseImpl(this.repository);
   @override
-  Future<Either<Failure, List<SearchGenres>>> call() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<SearchGenres>>> call() async {
+    return await repository.getGenresList();
   }
 }
--- movies/test/app/features/domain/usecases/search_genres_usecase_test.dart
+++ movies/test/app/features/domain/usecases/search_genres_usecase_test.dart
@@ -1,10 +1,23 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:movies/app/features/domain/entities/search_genres.dart';
 import 'package:movies/app/features/domain/repositories/search_genres_repository.dart';
 import 'package:movies/app/features/domain/usecases/search_by_text.dart';
 import 'package:movies/app/features/domain/usecases/search_genres_usecase.dart';
 
+class SearchGenresRepositoryMock extends Mock
+    implements SearchGenresRepository {}
+
 void main() {
-  final repository = SearchGenresRepository();
-  final usecase = SearchGenresUsecaseImpl();
-  test('Should receive a Search Genres list', () async {});
+  final repository = SearchGenresRepositoryMock();
+  final usecase = SearchGenresUsecaseImpl(repository);
+
+  List<SearchGenres> list = [];
+  test('Should receive a Search Genres list', () async {
+    when(() => repository.getGenresList()).thenAnswer((_) async => Right(list));
+    final result = await usecase.call();
+
+    expect(result, Right(list));
+  });
 }
