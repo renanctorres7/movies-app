dbb0a2e27be1bb0a88444aa996a4270fb6bbdc06
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-05-24 09:55:09 -0300

    Fix test error on Search Store test

--- movies/test/app/features/presenter/stores/search_store_test.dart
+++ movies/test/app/features/presenter/stores/search_store_test.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get/get.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:movies/app/core/utils/dependency_creator.dart';
 import 'package:movies/app/features/domain/entities/search_results.dart';
@@ -11,20 +10,20 @@ import 'package:movies/app/features/presenter/stores/search_store.dart';
 class SearchByTextMock extends Mock implements SearchByText {}
 
 void main() {
-  late SearchStore store;
-  late SearchByText usecase;
+  SearchStore? store;
+  SearchByText? usecase = SearchByTextMock();
   List<SearchResults> testList = [];
   String text = "vingadores";
 
   setUp(() {
-    usecase = SearchByTextMock();
-    store = Get.put(SearchStore(usecase: s1(), genresUsecase: s1()));
+    DependencyCreator.init();
+    store = SearchStore(usecase: usecase, genresUsecase: s1());
   });
 
   test('Should return a Search Results from the usecase', () async {
-    when(() => usecase.call(any())).thenAnswer((_) async => Right(testList));
+    when(() => usecase.call(text)).thenAnswer((_) async => Right(testList));
 
-    final result = await store.getListResults(text);
+    final result = await store?.getListResults(text);
 
     expect(result, Right(testList));
     verify(() => usecase(text)).called(1);
@@ -35,7 +34,7 @@ void main() {
     when(() => usecase.call(any()))
         .thenAnswer((_) async => Left(ServerFailure()));
 
-    final result = await store.getListResults(text);
+    final result = await store?.getListResults(text);
 
     expect(result, Left(ServerFailure()));
     verify(() => usecase(text)).called(1);
