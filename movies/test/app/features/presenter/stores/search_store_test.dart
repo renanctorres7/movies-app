6a163a416da97f7c2948a5686e1910cf88fe519e
Renan Campos Torres <67553146+renanctorres7@users.noreply.github.com>
2022-05-24 11:39:46 -0300

    Fix error test on 'Should return a Invalid Search Text from the usecase when there is an error'

--- movies/test/app/features/presenter/stores/search_store_test.dart
+++ movies/test/app/features/presenter/stores/search_store_test.dart
@@ -1,23 +1,27 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:movies/app/core/utils/dependency_creator.dart';
+
 import 'package:movies/app/features/domain/entities/search_results.dart';
 import 'package:movies/app/features/domain/errors/errors.dart';
 import 'package:movies/app/features/domain/usecases/search_by_text.dart';
+import 'package:movies/app/features/domain/usecases/search_genres_usecase.dart';
 import 'package:movies/app/features/presenter/stores/search_store.dart';
 
 class SearchByTextMock extends Mock implements SearchByText {}
 
+class SearchGenresUsecaseMock extends Mock implements SearchGenresUsecase {}
+
 void main() {
   SearchStore? store;
   SearchByText? usecase = SearchByTextMock();
+  SearchGenresUsecase? genresUsecase = SearchGenresUsecaseMock();
+
   List<SearchResults> testList = [];
   String text = "vingadores";
 
   setUp(() {
-    DependencyCreator.init();
-    store = SearchStore(usecase: usecase, genresUsecase: s1());
+    store = SearchStore(usecase: usecase, genresUsecase: genresUsecase);
   });
 
   test('Should return a Search Results from the usecase', () async {
@@ -29,14 +33,15 @@ void main() {
     verify(() => usecase(text)).called(1);
   });
 
-  test('Should return a Server Failure from the usecase when there is an error',
+  test(
+      'Should return a Invalid Search Text from the usecase when there is an error',
       () async {
     when(() => usecase.call(any()))
-        .thenAnswer((_) async => Left(ServerFailure()));
+        .thenAnswer((_) async => Left(InvalidSearchText()));
 
     final result = await store?.getListResults(text);
 
-    expect(result, Left(ServerFailure()));
+    expect(result, Left(InvalidSearchText()));
     verify(() => usecase(text)).called(1);
   });
 }
